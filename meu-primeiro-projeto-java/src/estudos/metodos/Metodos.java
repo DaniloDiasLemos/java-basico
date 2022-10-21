e984fb88a9ae336f5ad1b61bb137191262a9e921
DaniloDiasLemos <danilo-34@hotmail.com>
2022-10-20 21:15:32 -0300

    metodos

--- meu-primeiro-projeto-java/src/estudos/metodos/Metodos.java
+++ meu-primeiro-projeto-java/src/estudos/metodos/Metodos.java
@@ -2,8 +2,16 @@ package estudos.metodos;
 
 public class Metodos {
 
-    public static void main(String[] args) { 
-      
+    public static void main(String[] args) {
+        // metodos devem ser nomeados como verbo, camelCase
+        // public int somar(int n1, int n2){}; => retorna int
+        // public void abrirConexao(){}; => não retorna nada
+        // public double dividir(int dividendo, int divisor) throws Exception{};
+        //private void metodoPrivado(){} => só visivel para esta classe
+
+        
+
+
 
     }
 }
