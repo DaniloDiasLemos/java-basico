37fcfa4a8cb32c8215aca39cea00b0162644ec99
DaniloDiasLemos <danilo-34@hotmail.com>
2022-10-20 21:27:35 -0300

    SmartTv

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,27 @@
+/**
+ * SmartTv
+ */
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1, volume = 25;
+
+    public void ligar() {
+        ligada = true;
+    }
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+    
+}
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,28 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada ? : " + smartTv.ligada);
+        System.out.println("Canal atual? : " + smartTv.canal);
+        System.out.println("Volume atual? : " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV ligada ? : " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV ligada ? : " + smartTv.ligada);
+
+        smartTv.diminuirVolume();//24
+        smartTv.diminuirVolume();//23
+        smartTv.diminuirVolume();//22
+        smartTv.aumentarVolume();//23
+
+        System.out.println("Volume atual? : " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal atual? : " + smartTv.canal);
+        
+
+    }
+}
